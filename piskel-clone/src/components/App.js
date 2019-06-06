8640c10a0f1d96918edd9cbccbdaf9320e097046
deminiro <den.mironov2001@yandex.by>
2019-06-06 12:17:24 +0300

    refactor: rename file addNewFrame to actionWithFrames

--- piskel-clone/src/components/App.js
+++ piskel-clone/src/components/App.js
@@ -1,6 +1,6 @@
 import css from '../view/style';
 import canvas from './canvas/mainCanvas';
-import addNewFrame from './actionWithFrames/addNewFrame';
+import addNewFrame from './actionWithFrames/actionWithFrames';
 
 export default function app() {
   css();
